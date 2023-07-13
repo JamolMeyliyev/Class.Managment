7c915006d1eb7a3d7055cfbaf04a36da747bc1c5
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-13 15:57:57 +0500

    Identity_API

---
+++ Account.API/Context/IdentityDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Account.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Account.API.Context
+{
+	public class IdentityDbContext:DbContext
+	{
+		public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<User> Users => Set<User>();
+	}
+}
---
+++ Account.API/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Account.API.Middlewares
+{
+	// You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+	public class ErrorHandlerMiddleware
+	{
+		private readonly RequestDelegate _next;
+		private readonly ILogger<ErrorHandlerMiddleware> _logger;
+
+		public ErrorHandlerMiddleware(RequestDelegate next, ILogger<ErrorHandlerMiddleware> logger)
+		{
+			_next = next;
+			_logger = logger;
+		}
+
+		public async Task Invoke(HttpContext httpContext)
+		{
+			try
+			{
+				await _next(httpContext);
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e, "InternalError");
+
+				httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+				await httpContext.Response.WriteAsJsonAsync(new
+				{
+					Error = e.Message,
+				});
+			}
+		}
+	}
+
+	public static class ErrorHandlerMiddlewareExtensions
+	{
+		public static IApplicationBuilder UseErrorHandlerMiddleware(this IApplicationBuilder builder)
+		{
+			return builder.UseMiddleware<ErrorHandlerMiddleware>();
+		}
+	}
+}
